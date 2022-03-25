1ff5cee2feab47c59006dafa5170ff53c626e611
CheriseCodes <cherise.bry@gmail.com>
2022-03-24 23:16:00 -0400

    Begin linked list

---
+++ linked_list.h
@@ -0,0 +1,20 @@
+#ifndef LINKED_LIST_
+#define LINKED_LIST_
+
+template <typename T>
+class LLNode {
+    LLNode & prev_;
+    LLNode & next_;
+    T value_;
+    public:
+        LLNode(): prev_{0}, next_{0}, value_{} {};
+        LLNode(T value): value_{value} {};
+        LLNode(LLNode & next): next_{next} {};
+        LLNode(T value, & LLNode next): value_{value}, next_{next} {};
+        LLNode(T value, LLNode & next, LLNode & prev): value_{value}, next_{next}, prev_{prev} {};
+        T value() { return value_; }
+};
+
+
+
+#endif // LINKED_LIST_
