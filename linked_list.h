cc6edc40624cc03fee39f2e0ae5197383ea531ec
CheriseCodes <cherise.bry@gmail.com>
2022-03-24 23:37:33 -0400

    correct syntax error

--- linked_list.h
+++ linked_list.h
@@ -10,7 +10,7 @@ class LLNode {
         LLNode(): prev_{0}, next_{0}, value_{} {};
         LLNode(T value): value_{value} {};
         LLNode(LLNode & next): next_{next} {};
-        LLNode(T value, & LLNode next): value_{value}, next_{next} {};
+        LLNode(T value, LLNode & next): value_{value}, next_{next} {};
         LLNode(T value, LLNode & next, LLNode & prev): value_{value}, next_{next}, prev_{prev} {};
         T value() { return value_; }
 };
