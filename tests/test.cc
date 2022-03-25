8de051969a9c41d5702599469b4ddebcfe2d565e
CheriseCodes <cherise.bry@gmail.com>
2022-03-25 17:04:36 -0400

    Integrated Google Test

--- tests/test.cc
+++ tests/test.cc
@@ -1,7 +1,7 @@
-#include "gtest/gtest.h"
-#include "linked_list.h"
+#include <gtest/gtest.h>
+#include "../linked_list.h"
 
 TEST(DataStructures, IntializeEmptyLinkedList) {
-    LLNode ll = LLNode<int>();
+    LLNode<int> ll = LLNode<int>();
     EXPECT_EQ(ll.value(), 0);
 }
