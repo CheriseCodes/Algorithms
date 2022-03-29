1f1705ae5dd1dfac669d0a35be743653b21bebdb
CheriseCodes <cherise.bry@gmail.com>
2022-03-29 11:11:50 -0400

    Added LinkedList tests

--- tests/test.cc
+++ tests/test.cc
@@ -1,7 +1,38 @@
 #include <gtest/gtest.h>
 #include "../linked_list.h"
 
-TEST(DataStructures, IntializeEmptyLinkedList) {
-    LLNode<int> ll = LLNode<int>();
-    EXPECT_EQ(ll.value(), 0);
+TEST(LinkedList, IntializeEmptyNode) {
+    LLNode<int> lln = LLNode<int>();
+    EXPECT_EQ(lln.value(), 0);
 }
+
+TEST(LinkedList, SearchWithOneItem) {
+    LLNode<int> n = LLNode<int>(1);
+    LinkedList<int> ll = LinkedList<int>(&n);
+    LLNode<int> * found = ll.search(1);
+    EXPECT_EQ(found->value(), 1);
+}
+
+TEST(LinkedList, InsertIntoEmptyList) {
+    LinkedList<int> ll = LinkedList<int>();
+    ll.insert(1);
+    EXPECT_EQ(ll.root()->value(), 1); 
+}
+
+TEST(LinkedList, DeleteNodeFromListWithOneItem) {
+    LLNode<int> n = LLNode<int>(1);
+    LinkedList<int> ll = LinkedList<int>(&n);
+    ll.remove(&n);
+    EXPECT_TRUE(ll.is_empty());
+}
+
+TEST(LinkedList, DeleteKeyFromListWithOneItem) {
+    LLNode<int> n = LLNode<int>(1);
+    LinkedList<int> ll = LinkedList<int>(&n);
+    ll.remove(1);
+    EXPECT_TRUE(ll.is_empty());
+}
+
+
+
+
